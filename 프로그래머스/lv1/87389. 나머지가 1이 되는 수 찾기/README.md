1049118c8434fc3394bc26df1db690b03a67572d
5seung <126163816+5seung@users.noreply.github.com>
2023-08-31 16:58:29 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/README.md
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 나머지가 1이 되는 수 찾기 - 87389 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87389) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87389?language=java) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 0.02 ms
+메모리: 74.8 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -1,18 +1,13 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        int x = 1;
-        do{
-            if((n-1)%x == 0){
-                answer = x;
-            }
-            x++;
-        } while(answer == 1);
+       int answer = 1;
+
+        while(true) {
+            if (n % answer == 1)
+                break;
+            answer++;
+        }
 
         return answer;
     }
 }
-
-//1을 뺀 값의 약수 중 1이 아닌 가장 작은 값
-//9: 1,3,9
-//11: 1, 11
