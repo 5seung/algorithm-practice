b8f7f4a82c061b7f2325874752800051bc0c4d37
5seung <126163816+5seung@users.noreply.github.com>
2023-08-31 16:32:28 +0900

    Update 약수의 합.java

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
@@ -13,3 +11,6 @@ class Solution {
         return answer;
     }
 }
+
+//약수일때, 바로 answer에 누적해서 더하면되므로 배열까지 선언해서 쓸필요가 없다.
+//약수를 좀더 효율적으로 구하는 방법은 없을까? 
