ba4f333d6e59a26e420cbec246fa506a651b85d5
5seung <126163816+5seung@users.noreply.github.com>
2023-09-11 19:37:41 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.33 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,11 +2,7 @@ class Solution {
     public String solution(int n) {
         String answer = "";
         for(int i = 0; i < n; i++){
-            if(i%2==1){
-                answer += "박";
-                continue;
-            }
-            answer += "수";
+           answer += (i%2==1)? "박": "수";
         }
         return answer;
     }
