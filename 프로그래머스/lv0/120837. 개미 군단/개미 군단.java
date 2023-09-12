e97915ec6a87f338dd32fdc4a0c313d3faf505fe
5seung <126163816+5seung@users.noreply.github.com>
2023-09-12 22:48:01 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = hp/5;
+        hp %= 5;
+        answer += hp/3;
+        answer += hp%3; 
+        return answer;
+    }
+}
