d1b3a33d588085ba646bf251c8657a5035adf747
5seung <126163816+5seung@users.noreply.github.com>
2023-09-12 15:06:33 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -1,15 +1,12 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
         int harshad = x;
         int temp = 0;
         while(x>0){
             temp += x%10;
             x /= 10;
         }
-        if(harshad%temp!=0){
-            answer = false;
-        }
-        return answer;
+
+        return (harshad%temp!=0) ? false : true;
     }
 }
