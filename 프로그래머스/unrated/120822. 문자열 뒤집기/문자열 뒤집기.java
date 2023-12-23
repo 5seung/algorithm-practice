2891561ae0a8125bd8b807ff207b3c0efb7602a1
5seung <126163816+5seung@users.noreply.github.com>
2023-12-23 10:12:43 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,10 +1,7 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        char[] myArr = my_string.toCharArray();
-        for(int i = myArr.length-1 ; i>=0; i--){
-            answer += myArr[i];
-        }
+        StringBuilder sb = new StringBuilder(my_string);
+        String answer = sb.reverse().toString();
         return answer;
     }
 }
