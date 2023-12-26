bcbb64d625f41fb5611bdd8cbe1862890aaae815
5seung <126163816+5seung@users.noreply.github.com>
2023-12-26 11:42:13 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.53 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(numbers);
         int num1 = numbers[0]*numbers[1];
         int num2 = numbers[numbers.length-1]*numbers[numbers.length-2];
-        int answer = (num1>num2) ? num1 : num2;
+        int answer = Math.max(num1, num2);
         
         return answer;
     }
