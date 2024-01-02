64177e16d735676474e32d370a933c64309d5b77
5seung <126163816+5seung@users.noreply.github.com>
2024-01-02 20:48:10 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.31 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -3,19 +3,23 @@ import java.util.*;
 class Solution {
     public int[] solution(int k, int[] score) {
         int[] answer = new int[score.length];
-        int low = score[0];
-        // 명예의 전당 점수
-        int[] rank = new int[k];
-        for(int i = 0; i<score.length; i++){
-            // 명예 전당 최저점보다 높으면 명예 전당 등록
-            if(score[i] >= rank[0]){
-                rank[0] = score[i];
-                //오름차순 정렬 : 낮은 점수 ~ 높은 점수
-                Arrays.sort(rank);
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        int temp = 0;
+
+        for(int i = 0; i < score.length; i++) {
+
+            priorityQueue.add(score[i]);
+            if (priorityQueue.size() > k) {
+                priorityQueue.poll();
             }
-            low = (i < k) ? rank[k-i-1] : rank[0];
-            answer[i] = low;
+
+            answer[i] = priorityQueue.peek();
         }
+
+
+
         return answer;
     }
 }
