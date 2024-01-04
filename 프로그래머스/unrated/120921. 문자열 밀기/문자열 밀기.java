d25c63375a09b8a4c5a3b3ce231d6de2f2f75854
5seung <126163816+5seung@users.noreply.github.com>
2024-01-04 17:55:00 +0900

    [level unrated] Title: 문자열 밀기, Time: 1.28 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.java
@@ -1,13 +1,7 @@
 class Solution {
     //A를 밀어서 B가 될 수 있다면 = A: 원본 문자열
     public int solution(String A, String B) {
-        int answer = 0;
-        for(int i = 0; i<A.length(); i++){
-            if(A.contains(B.substring(i))){
-                answer = B.substring(0,i).equals(A.substring(A.length()-i,A.length())) ? i : -1;
-                break;
-            }
-        }
+        int answer = (B+B).indexOf(A);
         return answer;
     }
 }
