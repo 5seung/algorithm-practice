a5d42644a23ebd89d68a59be091b421da1f2d299
5seung <126163816+5seung@users.noreply.github.com>
2024-01-08 10:28:16 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,15 +1,14 @@
 class Solution {
     public int solution(int num) {
         int answer = -1;
+        
         long Collatz = (long)num;
-        int cnt = 0;
-        for(int i = 0; i<500; i++){
+        for(int i = 0; i < 500; i++){
             if(Collatz == 1){
-                answer = cnt;
+                answer = i;
                 break;
             }
             Collatz = (Collatz%2 == 0) ? Collatz/2 : (Collatz*3)+1;
-            cnt++;
         }
         
         return answer;
