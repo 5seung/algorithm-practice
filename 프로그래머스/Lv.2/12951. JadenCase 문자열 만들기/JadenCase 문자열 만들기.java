3dfcf14d930100d1418d6ca33fad603a1c555ef4
5seung <126163816+5seung@users.noreply.github.com>
2024-01-15 20:19:31 +0900

    [level Lv.2] Title: JadenCase 문자열 만들기, Time: 1.20 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/Lv.2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,23 +1,14 @@
 class Solution {
     public String solution(String s) {
-        String[] temp = s.toLowerCase().split(" ");
-        String answer = "";
-        for(String t : temp){
-            // 공백문자가 연속해서 나올 경우 처리
-            if(t.length() == 0){
-                answer += " ";
-                continue;
-            }
-            
-            // 0번째 문자는 대문자로
-    	    answer += t.substring(0, 1).toUpperCase();
-    		// 1번째 문자부터 마지막 문자
-    		answer += t.substring(1, t.length());
-            
-            if(s.length() != answer.length()){
-                answer += " ";
-            }
+         String answer = "";
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for(String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
         }
+
         return answer;
     }
 }
