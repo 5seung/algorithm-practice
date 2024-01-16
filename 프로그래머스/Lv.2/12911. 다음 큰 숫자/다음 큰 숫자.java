9d46d7a498eba980a9d72c04bb45473a4af2e6a7
5seung <126163816+5seung@users.noreply.github.com>
2024-01-16 10:53:00 +0900

    [level Lv.2] Title: 다음 큰 숫자, Time: 0.02 ms, Memory: 59.8 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/Lv.2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,11 +1,15 @@
 class Solution {
     public int solution(int n) {
         int answer = n+1;
-        int a = Integer.toString(n, 2).replace("0", "").length();
-        int b = Integer.toString(answer, 2).replace("0", "").length();
+        // int a = Integer.toString(n, 2).replace("0", "").length();
+        // int b = Integer.toString(answer, 2).replace("0", "").length();
+        // > Integer.bitCount()를 통해 간단히 1의 개수를 찾을 수 있다.
+        // : 주어진 정수에서 true bit의 개수를 찾는 함수
+        int a = Integer.bitCount(n);
+        int b = Integer.bitCount(answer);
         while(a != b){
             answer++;
-            b = Integer.toString(answer, 2).replace("0", "").length();
+            b = Integer.bitCount(answer);
         }
         return answer;
     }
