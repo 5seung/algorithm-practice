1fb6f06ec83eb468ecd2c2f0897cb6aae11b8592
5seung <126163816+5seung@users.noreply.github.com>
2024-01-21 16:24:57 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,24 +1,27 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] arr) {
-        Arrays.sort(arr);
-        int answer = arr[arr.length-1];
-        int temp = 1;
-        Boolean bool = true;
-        while(bool){
-            for(int i : arr){
-                if(answer%i != 0){
-                    bool = true;
-                    temp++;
-                    break;
-                }else{
-                    bool = false;
-                }
-            }
-            answer = arr[arr.length-1]*temp;
+        int answer = 1;
+
+        for(int i=0;i<arr.length;i++) {
+            answer = lcm(answer, arr[i]);
         }
-            
+
         return answer;
     }
+    
+    // 최소공배수 : 두 수를 곱해 최대공약수로 나누면 최소공배수가 된다. 
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    // 최대공약수 : '유클리드 호제법'
+    // 1. 큰수를 작은수로 나눈다.
+    // 2. 나누는 수를 나머지로 나눈다 > 나머지가 0이될때, 나눈수가 최대공약수
+    static int gcd(int a, int b) {
+        if(a % b == 0) {
+            return b;
+        }
+        return gcd(b, a % b);
+    }
+
 }
