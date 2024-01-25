a4ac3c771b561fc1d125504161d4aac5dd1f0539
5seung <126163816+5seung@users.noreply.github.com>
2024-01-25 18:01:07 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,18 +2,20 @@ import java.util.*;
 
 // 할인하는 제품은 하루에 하나씩만 구매할 수 있다.
 // 원하는 제품을 모두 할인 받을 수 있는 회원등록 날짜의 총 일수
+// 슬라이딩 윈도우 알고리즘을 활용하면 더 효율적으로 구현가능
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        int leftIdx = 0;
+        int rightIdx = 9;
+        Map<String, Integer> dc = new HashMap<>();
+        // 1~10일 할인 물품 목록
+        for(int i = 0; i < 10; i++){
+            dc.put(discount[i], dc.getOrDefault(discount[i],0)+1);
+        } 
         
-        for(int i = 0; i < discount.length-9; i++) {
-            // 10일간 할인 품목 map에 넣기
-            Map<String, Integer> dc = new HashMap<>();
-            for(int j = i; j < i+10; j++) {
-                dc.put(discount[j], dc.getOrDefault(discount[j], 0) + 1);
-            }
-            
-            // number와 map의 값을 비교 
+        while(rightIdx < discount.length){
+            // 가원가입 가능 여부 체크
             boolean flag = true;
             for(int j = 0; j < want.length; j++) {
                 // map에 want 물품이 없거나, want 값이 크면 탐색 종료
@@ -22,12 +24,19 @@ class Solution {
                     break;
                 }
             }
-        
             if(flag) {
                 answer++;
             }
-            
+
+            // 다음 가입일 할인 물품 map 셋팅
+            rightIdx++;
+            if(rightIdx < discount.length){
+                dc.put(discount[rightIdx], dc.getOrDefault(discount[rightIdx],0)+1);
+                dc.put(discount[leftIdx], dc.get(discount[leftIdx])-1);
+                leftIdx++;
+            }
         }
+
         return answer;
     }
 }
