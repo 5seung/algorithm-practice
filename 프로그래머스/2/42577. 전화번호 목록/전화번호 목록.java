56fd15775ac3793675e0e6852b7211bd5ec759ee
5seung <126163816+5seung@users.noreply.github.com>
2024-02-03 08:38:30 +0900

    [level 2] Title: 전화번호 목록, Time: 323.70 ms, Memory: 95.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        
+        boolean answer = true;
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            String s = phone_book[i];
+            if (phone_book[i + 1].startsWith(s)) {
+                answer = false;
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
