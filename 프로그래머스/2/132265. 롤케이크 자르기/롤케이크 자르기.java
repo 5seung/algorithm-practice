f130a391ad9363932b3d776c47048b8ec26f2c70
5seung <126163816+5seung@users.noreply.github.com>
2024-02-19 21:38:14 +0900

    [level 2] Title: 롤케이크 자르기, Time: 181.87 ms, Memory: 168 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+        int len = topping.length;
+        
+        HashSet<Integer> first = new HashSet<>();
+        HashMap<Integer, Integer> second = new HashMap<>();
+        
+        first.add(topping[0]);
+        for (int i = 1; i < len; i++) {
+            second.put(topping[i], second.getOrDefault(topping[i], 0) + 1);
+        }
+        
+        for (int i = 1; i < len; i++) {
+            first.add(topping[i]);
+            second.put(topping[i], second.get(topping[i]) - 1);
+            if (second.get(topping[i]) == 0) {
+                second.remove(topping[i]);
+            }
+            if (first.size() == second.size()) answer++;
+        }
+        
+        return answer;
+    }
+}
