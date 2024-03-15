296fd841998082fa7f24c2c3d827438e0c2a516a
5seung <126163816+5seung@users.noreply.github.com>
2024-03-15 15:12:08 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,8 @@
+-- Front End 스킬을 가진 개발자의 정보를 조회
+SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
+FROM DEVELOPERS
+WHERE SKILL_CODE & (
+        SELECT SUM(CODE)
+        FROM SKILLCODES
+        WHERE CATEGORY = 'Front End')
+ORDER BY ID
